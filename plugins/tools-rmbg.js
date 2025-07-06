2ee616cf67ef3a4cc704615d1dd67a87ff7dfef5
Tomi Lucky <123148702+Tomilucky218@users.noreply.github.com>
2025-07-06 15:51:33 +0300

    Update tools-rmbg.js

--- plugins/tools-rmbg.js
+++ plugins/tools-rmbg.js
@@ -75,7 +75,7 @@ malvin({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `Background removed\n\n> *Powered by LUCKY-XD*`
+      caption: `Background removed\n\n> *Powered By Lucky Tech Hub*`
     });
 
   } catch (error) {
