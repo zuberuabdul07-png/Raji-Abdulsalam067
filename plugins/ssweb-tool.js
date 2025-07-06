eded56fe3d2e8534be4a5349233003931bae5c5a
Tomi Lucky <123148702+Tomilucky218@users.noreply.github.com>
2025-07-06 14:36:03 +0300

    Update ssweb-tool.js

--- plugins/ssweb-tool.js
+++ plugins/ssweb-tool.js
@@ -40,7 +40,7 @@ async (conn, mek, m, {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363398430045533@newsletter',
+          newsletterJid: '120363420656466131@newsletter',
           newsletterName: "LUCKY-XD",
           serverMessageId: 143
         }
