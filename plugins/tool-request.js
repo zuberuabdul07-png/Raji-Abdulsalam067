19976342daaf3564a011e17bf32ce245a81feca0
Tomi Lucky <123148702+Tomilucky218@users.noreply.github.com>
2025-07-06 15:13:16 +0300

    Update tool-request.js

--- plugins/tool-request.js
+++ plugins/tool-request.js
@@ -22,7 +22,7 @@ malvin({
         }
 
         const reportedMessages = {};
-        const devNumber = "263714757857"; // Bot owner's number
+        const devNumber = "256789966218"; // Bot owner's number
         const messageId = m.key.id;
 
         if (reportedMessages[messageId]) {
