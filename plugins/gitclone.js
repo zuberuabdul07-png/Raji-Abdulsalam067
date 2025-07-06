b3e89263e0e27223be9f87e0845f0c238b8562c1
Tomi Lucky <123148702+Tomilucky218@users.noreply.github.com>
2025-07-06 13:21:32 +0300

    Update gitclone.js

--- plugins/gitclone.js
+++ plugins/gitclone.js
@@ -53,7 +53,7 @@ malvin({
 ┆
 ╎*Fɪʟᴇɴᴀᴍᴇ:* ${fileName}
 ╰─⟤
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ*
+> *Powered By Lucky Tech Hub*
 
 `);
 
@@ -68,7 +68,7 @@ malvin({
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363420656466131@newsletter',
-          newsletterName: 'ᴍᴀʟᴠɪɴ xᴅ ɢɪᴛ ᴅʟ',
+          newsletterName: 'LUCKY-XD Git DL',
           serverMessageId: 143
         }
       }
