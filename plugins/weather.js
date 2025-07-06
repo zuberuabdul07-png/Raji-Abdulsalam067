37e1cfdc3d352e89da4f76cb1acea5f5f054652f
Tomi Lucky <123148702+Tomilucky218@users.noreply.github.com>
2025-07-06 16:04:57 +0300

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By malvin*
+> *© Powered By Lucky Tech Hub*
 `;
         return reply(weather);
     } catch (e) {
